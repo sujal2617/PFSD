829b6165a6f47b67204cd7d53ae3923aafc1bf78
sujal2617 <2200032771@kluniversity.in>
2024-02-03 15:26:14 +0530

    make changes in settings.py

--- PetParadise/PetParadise/settings.py
+++ PetParadise/PetParadise/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [                dhkdhgvhbhjkjnkjbkjbkjokjkj
+INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
