9b1428791d06848d2f7dce99a4b30292f3da49a7
sujal2617 <2200032771@kluniversity.in>
2024-02-05 10:54:20 +0530

    make changes in settings.py

--- PetParadise/PetParadise/settings.py
+++ PetParadise/PetParadise/settings.py
@@ -8,8 +8,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
-"""                                   sujal pattewar
-
+"""
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
