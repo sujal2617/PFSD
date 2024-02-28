a84bbb85414506fa775252ea72f00182d965825f
sujal2617 <2200032771@kluniversity.in>
2024-02-29 00:23:08 +0530

    CHANGES MADE ON 28-02-2024

---
+++ PetParadise/PetParadise/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+
+def homepage(request):
+    return render(request, "index.html")
+
+def aboutpage(request):
+    return render(request, "about.html")
+
+def contactpage(request):
+    return render(request, "contact.html")
+
