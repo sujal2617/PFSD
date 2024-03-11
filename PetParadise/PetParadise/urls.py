7d2611a6e5dfba0a311c71db523d50d3fdee0dce
sujal2617 <sujalpattewar26@gmail.com>
2024-03-11 19:54:35 +0000

    final from pa

--- PetParadise/PetParadise/urls.py
+++ PetParadise/PetParadise/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 urlpatterns = [
@@ -41,3 +44,5 @@ urlpatterns = [
 
 
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- PetParadise/adminapp/urls.py
+++ PetParadise/adminapp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 urlpatterns=[
@@ -11,3 +14,5 @@ urlpatterns=[
     path('addtocart/<str:item_name>/<int:item_cost>/', views.addtocart, name='addtocart'),
 
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
